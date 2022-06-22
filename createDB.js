90427484ba43f057d20ef1fba32ffbe1fbcbf81d
ahsas13 <ahsas008@gmail.com>
2022-06-23 00:35:36 +0300

    br_6.1_6.3

---
+++ createDB.js
@@ -0,0 +1,20 @@
+var MongoClient = require('mongodb').MongoClient
+var data = require("./data.js").data 
+
+const uri = "mongodb://localhost:27017/"
+const client = new MongoClient(uri)
+async function run() {
+    try  {
+        await client.connect();
+        var database = client.db("smeshariki");
+        database.dropDatabase()
+        database = client.db("smeshariki");
+        const heroes = database.collection("heroes");
+        const result = await heroes.insertMany(data);
+        console.log(`${result.insertedCount} documents were inserted`);
+
+    } finally {
+        await client.close();
+    }
+}
+run()
